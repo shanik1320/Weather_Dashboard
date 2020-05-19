29a2ed9aa816757db5b96eac480fc8e9aeaada13
shanik hernandez <shaniik.her93@gmail.com>
2020-05-19 17:24:34 -0400

    changed header

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //add the weather api
-var APIKey = "08f799c58a6f983cf5f271232c7ecd27";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "q={city},{state}&appid=" + APIKey;
+var APIKey = "c170d70d54f7a96f7a41dd23b50e6768";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=" +APIKey;
+// "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state}&appid=" + APIKey
+// "https://api.openweathermap.org/data/2.5/forecast?" + "q={city},{state}&appid=" + APIKey;
 
 
 $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  })
+  .then(function(response) {
+    console.log(queryURL);
+
       console.log(response);
+    //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    //     $(".wind").text("Wind Speed: " + response.wind.speed);
+    //     $(".humidity").text("Humidity: " + response.main.humidity);
   })
