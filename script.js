063237713f4183cc0ef8074cefb70203da80918f
shanik hernandez <shaniik.her93@gmail.com>
2020-05-19 18:28:02 -0400

    added function

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-//add the weather api
-var APIKey = "c170d70d54f7a96f7a41dd23b50e6768";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=" +APIKey;
-// "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state}&appid=" + APIKey
-// "https://api.openweathermap.org/data/2.5/forecast?" + "q={city},{state}&appid=" + APIKey;
+$(document).ready(function(){
+  var APIKey = "218f7f29ea28f9c27590845f89e27aae";
 
+  $("#submit").on("click", getWeather);
+  $(document).on("click", "history", function () {
+    var userCity =$(this).data("city");
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&units=imperial&appid=" + APIKey;
+    weatherAjaxCall(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
   })
-  .then(function(response) {
-    console.log(queryURL);
+  function getWeather(e) {
+    
+    e.preventDefault();
+   
+    var userCity = $("#city-input").val();
+    var userCity = userCity.toLowerCase();
+ 
+    localStorage.setItem(userCity, userCity);
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=imperial&appid=" + APIKey;
+    weatherAjaxCall(queryURL);
+    renderCities();
+  }
 
-      console.log(response);
-    //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    //     $(".wind").text("Wind Speed: " + response.wind.speed);
-    //     $(".humidity").text("Humidity: " + response.main.humidity);
-  })
+
+
+})
