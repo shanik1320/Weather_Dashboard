c32d440014068a14f79423b63ce49cbec8c238a9
shanik hernandez <shaniik.her93@gmail.com>
2020-05-19 20:25:40 -0400

    changed the call

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
-  var APIKey = "218f7f29ea28f9c27590845f89e27aae";
+  // var APIKey = "218f7f29ea28f9c27590845f89e27aae";
 
-  $("#submit").on("click", getWeather);
-  $(document).on("click", "history", function () {
-    var userCity =$(this).data("city");
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&units=imperial&appid=" + APIKey;
-    weatherAjaxCall(queryURL);
+  $(".btn").click(function(){
+    var city =$("#city").val();
 
+    if(city != ""){
+      $.ajax({
+        url:"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=218f7f29ea28f9c27590845f89e27aae",
+        type: "GET",
+      }).then(function(reponse){
+        console.log(reponse)
+      })
+    }else{
+      $("#error").html("Field cannot be empty")
+    }
   })
-  function getWeather(e) {
-    
-    e.preventDefault();
-   
-    var userCity = $("#city-input").val();
-    var userCity = userCity.toLowerCase();
- 
-    localStorage.setItem(userCity, userCity);
-    
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=imperial&appid=" + APIKey;
-    weatherAjaxCall(queryURL);
-    renderCities();
-  }
+});
 
-
-
-})
+function weather(reponse)
